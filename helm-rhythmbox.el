c92e1ded34ddd4e62e7e9a558259c232e05193fa
Thomas Winant <thomas.winant@cs.kuleuven.be>
2016-05-24 13:58:03 +0200

    Two spaces after periods in elisp files

--- helm-rhythmbox.el
+++ helm-rhythmbox.el
@@ -25,10 +25,10 @@
 
 ;; Control Rhythmbox's play queue via Helm.
 ;;
-;; Start by calling `helm-rhythmbox' to browse the library retrieved
-;; from a running Rhythmbox instance via D-Bus. The default action is
-;; to play the selected song. There is also the option to enqueue the
-;; selected song, this also works for a selection.
+;; Start by calling `helm-rhythmbox' to browse the library retrieved from a
+;; running Rhythmbox instance via D-Bus.  The default action is to play the
+;; selected song.  There is also the option to enqueue the selected song, this
+;; also works for a selection.
 ;;
 ;; The whole library is retrieved on first use and can be reloaded
 ;; with `helm-rhythmbox-reload-library'.
